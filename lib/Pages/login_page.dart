ba9f988432d8760cbd0f63805a0b95ec8370f266
BaioumyHussain <bibohussain8@gmail.com>
2024-12-06 02:53:12 +0200

    great work

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 //nop
 //login from abdo
 //login from baioumy 
+//great work
 class LoginPage extends StatelessWidget {
   //email and pw text controllers
   final TextEditingController _emailcontroller = TextEditingController();
