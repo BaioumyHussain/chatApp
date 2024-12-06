4e3b740d6a070f9e596a415159039bb9b4127a5b
BaioumyHussain <bibohussain8@gmail.com>
2024-12-06 02:00:24 +0200

    splash_Baioumy

--- lib/Pages/register_page.dart
+++ lib/Pages/register_page.dart
@@ -2,7 +2,7 @@ import 'package:chatapp/Components/my_buttons.dart';
 import 'package:chatapp/Components/my_textfield.dart';
 import 'package:chatapp/Services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-
+//register page
 class RegisterPage extends StatelessWidget {
   final TextEditingController _emailcontroller = TextEditingController();
   final TextEditingController _pwcontroller = TextEditingController();
@@ -23,6 +23,7 @@ class RegisterPage extends StatelessWidget {
                   title: Text(e.toString()),
                 ));
       }
+    
     } else {
       showDialog(
           context: context,
